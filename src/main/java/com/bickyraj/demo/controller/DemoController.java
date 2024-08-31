8698b127705a6ec571c6393170538f481363dd5e
bickyraj <bickyrajkayastha@gmail.com>
2024-08-31 10:27:47 +0200

    updates

--- src/main/java/com/bickyraj/demo/controller/DemoController.java
+++ src/main/java/com/bickyraj/demo/controller/DemoController.java
@@ -9,7 +9,7 @@ public class DemoController {
     @RequestMapping("")
     @ResponseBody
     public String index() {
-        return "Welcome to stock market new world heheeh";
+        return "Welcome to stock market new world heheeh haha";
     }
     @RequestMapping("/hello")
     @ResponseBody
